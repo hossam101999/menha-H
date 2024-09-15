7ba4bd58007bf368053a1f4d15185cb6c5e6c232
Amr Mosad Kataria <159139472+Amrkataria@users.noreply.github.com>
2024-09-15 13:20:18 +0300

    FEAT:add repeat paragraph (#1)

---
+++ src/ui/RepeatParagrah.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function RepeatParagrah({ children }) {
+  return (
+    <div className="font-normal  text-base leading-snug font-brush text-[#8A690F]">
+      {children}
+    </div>
+  );
+}
